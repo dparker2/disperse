efbc1260ce02cf3fc12f097afeaec3a53f9c8253
Rumia <rumia_zhihu@hotmail.com>
2019-12-31 00:30:02 +0800

    Add join() for worker thread

--- src/worker/worker.cpp
+++ src/worker/worker.cpp
@@ -3,9 +3,11 @@
 
 #include <queue>
 #include <mutex>
+#include <atomic>
 
 std::mutex mutex;
 std::queue<Tailor::command> queue;
+std::atomic<bool> quit(false);
 
 void workerMain() {
     while (true) {
@@ -17,11 +19,19 @@ void workerMain() {
         }
         mutex.unlock();
         API::sleep(50);
+        if(quit) {
+            break;
+        }
     }
 }
 
 Worker::Worker() {
     this->thread_obj = std::thread(workerMain);
+}
+
+Worker::~Worker() {
+    quit = true;
+    this->thread_obj.join();
 }
 
 void Worker::process(Tailor::command data) {
